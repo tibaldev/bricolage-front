1f629cc6e87f5a98a39c9d048263da5e1efa4a0e
thibaudt <t.tallon@dispofi.fr>
2015-03-13 17:52:32 +0100

    début travail tpls coté serveur

--- controller/index.js
+++ controller/index.js
@@ -1,4 +1,26 @@
-var data = require(__base + 'json/data.json');
+/**
+ * @todo
+ * récupérer les mots clés automatiquement avec un système de doclet en entête du tpl :
+ * @tpl_name     : node-var-to-angular
+ * @tpl_keywords : nodejs, angular, preload
+ */
+
+
+/**
+ * @todo
+ * node : lister les fichiers tpls dans la vue
+ * node : récupérer pour chaque fichier les infos doclet
+ * node : insérer les données formatées dans le tableau data
+ * angular : les afficher dans la vue avec ng-repeat et ng-include
+ */
+
+var data  = {
+  keywords : [ 'angular', 'nodejs', 'css', 'npm', 'bower', 'bash' ],
+  tpls : ['node-var-to-angular']
+}
+
+
+
 
 exports.index = function (req, res) {
   res.render('index', { data: data });
--- public/js/controller.app.js
+++ public/js/controller.app.js
@@ -1,12 +1,26 @@
-var app = angular.module('app', ['gs.preloaded', 'ui.bootstrap']);
+var app = angular.module('app', ['gs.preloaded', 'ui.bootstrap'], function ($interpolateProvider) {
+  // Conflits avec le template Swig, on remplace la syntaxe Angular {{ }} par [[ ]]
+  $interpolateProvider.startSymbol('[[');
+  $interpolateProvider.endSymbol(']]');
+}); 
 
 
 // récupération des data passées par le serveur node
 app.service('locals', ['$preloaded', function($preloaded){
   this.keywords = $preloaded.keywords;
+  this.tpls = $preloaded.tpls;
 }])
 
 
-app.controller('KeywordSearch', ['$scope', 'locals', function($scope, locals) {
+app.controller('KeywordSearch', ['$scope', 'locals', function ($scope, locals) {
   $scope.keywords = locals.keywords;
+}]);
+
+app.controller('KeywordList', ['$scope', 'locals', function ($scope, locals) {
+  $scope.keywords = locals.keywords;
+}]);
+
+app.controller('Main', ['$scope', 'locals', function ($scope, locals) {
+  $scope.tpls = locals.tpls;
+  console.log($scope.tpls);
 }]);
